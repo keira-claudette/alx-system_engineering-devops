4c559c333621675367d47777652b25aea39221e3
keira-claudette <claudette.keira@gmail.com>
2021-08-13 17:27:51 +0000

    correction: added provider and return to exec

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
-file {  '/tmp/holberton':
-  path    => '/tmp/holberton'
-  content => 'I love Puppet',
-  group   => 'www-data',
-  owner   => 'www-data',
+# Using Puppet, create a file in '/tmp'.
+file { '/tmp/holberton':
+  ensure  => 'file',
+  path    => '/tmp/holberton',
   mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,7 @@
 # https://puppet.com/docs/puppet/3.8/type.html#exec
-exec {  'kill process':
-  command => 'pkill killmenow'
+# Kills a process
+exec {  'killmenow':
+  command  => '/usr/bin/pkill killmenow',
+  provider => 'shell',
+  returns  => [0, 1],
 }
