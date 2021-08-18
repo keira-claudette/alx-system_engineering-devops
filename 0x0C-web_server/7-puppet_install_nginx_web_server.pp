1a33c5914b397c699917d254ac5169fb36ee7fae
keira-claudette <claudette.keira@gmail.com>
2021-08-18 19:08:45 +0000

    initial commit

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,26 @@
+# A puppet manifest that:
+#			 1. Installs nginx
+#			 2. Redirects url(redirect_me) to a youtube page
+#			 3. Creates and configures nginx to use a custom
+#			    error 404 page.
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'redirecting':
+    ensure => 'present',
+    path   => 'etc/nginx/sites-available/default',
+    after  => 'listen 80 defualt_server;',
+    line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+}
+
+file { 'landing page':
+    path    => '/var/www/html/index.html':
+    content => 'Holberton School',
+}
+
+service {'nginx':
+  ensure  => running,
+  require => package['nginx'],
+}
