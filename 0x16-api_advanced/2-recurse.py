617a74d43124aed0ad065a2c1e533474e766e526
keira-claudette <claudette.keira@gmail.com>
2021-09-28 21:40:47 +0000

    solved pep8 errors

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -19,9 +19,9 @@ def recurse(subreddit, hot_list=[], count=0, after=None):
         return None
 
     full_hotlst = hot_list + [child.get("data").get("title")
-                               for child in response.json()
-                               .get("data")
-                               .get("children")]
+                              for child in response.json()
+                              .get("data")
+                              .get("children")]
     page_info = response.json()
     if not page_info.get("data").get("after"):
         return full_hotlst
