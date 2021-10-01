f2769793e6def9490cb107d5fbd5bf6e3d0b4460
keira-claudette <claudette.keira@gmail.com>
2021-10-01 09:04:49 +0000

    parser validated code

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # Debugs a wordpress site running on apache2.
-exec { 'fixes http error 500'}:
+exec { 'fixes http error 500':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+  # command => 'sed -i s//languages///g /var/www/html/wp-includes/class-wp-theme.php; sudo service apache2 restart',
+  path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin','/bin']
 }
--- 0x17-web_stack_debugging_3/README.md
+++ 0x17-web_stack_debugging_3/README.md
@@ -1,3 +1,8 @@
 # Web stack debugging
 - Debugging a Wordpress website running on a LAMP stack.
 - Automating the process using puppet
+
+###### Tip
+``` puppet parser validate some_file.pp
+    puppet-lint some_file.pp
+```
